1b2d3c3979774cf5b3c8ca0febd7c63d92787077
Jeremy Junac <jeremy.junac@etu.unice.fr>
2018-09-26 10:58:04 +0200

    add merge sort

---
+++ lib/complexity/sorts/merge.rb
@@ -0,0 +1,38 @@
+class Insertion
+
+    def initialize
+    end
+
+    def sort(arr)
+        if arr.length <= 1
+            return
+        end
+        mid = arr.length / 2
+        left = sort arr(0...mid)
+        right = sort arr(mid...arr.length - mid)
+
+        return merge(left, right)
+    end
+
+    def merge(left, right)
+        res = []
+
+        until left.empty? or right.empty?
+            if left.first <= right.first
+                res << left.shift
+            else
+                res << right.shift
+            end
+        end
+
+        unless left.empty?
+            res += left
+        end
+        unless right.empty?
+            res += right
+        end
+
+        return res
+    end
+
+end
