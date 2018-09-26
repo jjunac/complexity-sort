db28e8f216c16c628ba66df5f693f03ac949f4b7
Jeremy Junac <jeremy.junac@etu.unice.fr>
2018-09-26 10:14:32 +0200

    add insertion sort

--- lib/complexity/sorts/insertion.rb
+++ lib/complexity/sorts/insertion.rb
@@ -1,2 +1,24 @@
 class Insertion
+
+    def initialize
+    end
+
+    def sort!(arr)
+        i = 1
+        while i < arr.length
+            x = arr[i]
+            j = i - 1
+            while j >= 0 and arr[j] > x
+                arr[j+1] = arr[j]
+                j -= 1
+            end
+            arr[j+1] = x
+            i += 1
+        end
+    end
+
+    def sort(arr)
+        return sort! arr.map(&:clone)
+    end
+
 end
