7db9472e6a819e06be84519214da953028f1d52b
Thomas <canava.thomas@gmail.com>
2018-09-26 12:08:59 +0200

    first version of tester

--- lib/complexity/tester/tester.rb
+++ lib/complexity/tester/tester.rb
@@ -1,23 +1,42 @@
-class Tester
-  NUMBER_MAX = 1000
-  REPEAT = 1000
-  MAX_LEN = 16
+require 'benchmark'
 
-  def initialize
+class Array
+  def sum
+    inject(0.0) {|result, el| result.real + el.real}
   end
+
+  def mean
+    sum / size
+  end
+end
+
+class Tester
+
+  def initialize(number_max = 1000, repeat = 1000, max_len = 8)
+    @number_max = number_max
+    @repeat = repeat
+    @max_len = max_len
+  end
+
 
   def execute(sort_algorithm)
-    for n in 1..MAX_LEN
+    times = []
+    for n in 1..@max_len
       array = generate_random(2 ** n)
-      for i in 0...REPEAT
-        start = Time.now
-        sort_algorithm array
-        diff = Time.now - start
+      times[n - 1] = []
+      for i in 0...@repeat
+        time = Benchmark.measure {sort_algorithm.sort(array)}
+        times[n - 1] << time
       end
+      p times[n - 1][0].real
+      mean = times[n - 1].mean
+      p "The mean time for #{2 ** n} random values is #{mean}"
     end
+    times
   end
 
+
   def generate_random(size)
-    (0...size).map(rand(NUMBER_MAX))
+    (0...size).map {rand(@number_max)}
   end
 end
---
+++ main.rb
@@ -0,0 +1,5 @@
+require './lib/complexity/tester/tester'
+require './lib/complexity/sorts/insertion'
+
+tester = Tester.new
+tester.execute(Insertion.new)
