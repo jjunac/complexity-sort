ec38aebe0c4f5ffe7dae01508e15f092ffaf5d2e
Thomas <canava.thomas@gmail.com>
2018-10-03 09:41:30 +0200

    added covariance

--- plotter/plotter.py
+++ plotter/plotter.py
@@ -4,17 +4,18 @@ import argparse
 import plotly.graph_objs as go
 from plotly.offline import plot, iplot
 import math
-
+import numpy
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--csv",help="The csv that needs to be plotted",
-default='../test.csv')
+parser.add_argument("--csv", help="The csv that needs to be plotted",
+                    default='../test.csv')
 args = parser.parse_args()
 
+
 def read_csv(csv_file):
-    with open(csv_file,"r") as f:
+    with open(csv_file, "r") as f:
         reader = csv.DictReader(f)
-        d = {key:[] for key in reader.fieldnames}
+        d = {key: [] for key in reader.fieldnames}
         rows = [row for row in reader]
         for key in reader.fieldnames:
             print(key)
@@ -22,8 +23,18 @@ def read_csv(csv_file):
                 d[key].append(row[key])
         return d
 
-df = read_csv(args.csv)
 
+df = read_csv(args.csv)
+exponents = {}
+for k in df:
+    if k != "array size":
+        xmap = list(map(int, df["array size"]))
+        x = numpy.log(numpy.asarray(xmap))
+        ymap = list(map(float, df[k]))
+        y = numpy.log(numpy.asarray(ymap))
+        cov = numpy.cov(x, y)
+        exponents[k] = cov[1][0] / cov[0][0]
+print(exponents)
 traces = []
 for k in df:
     if k != "array size":
@@ -33,14 +44,14 @@ for k in df:
         traces.append(trace)
 
 layout = go.Layout(title='Simple Plot from csv data',
-                     xaxis=dict(
-                        type='log',
-                        autorange=True
-                    ),
-                    yaxis=dict(
-                        type='log',
-                        autorange=True
-                    ),
+                   xaxis=dict(
+                       type='log',
+                       autorange=True
+                   ),
+                   yaxis=dict(
+                       type='log',
+                       autorange=True
+                   ),
                    plot_bgcolor='rgb(230, 230,230)')
 #
 fig = go.Figure(data=traces, layout=layout)
